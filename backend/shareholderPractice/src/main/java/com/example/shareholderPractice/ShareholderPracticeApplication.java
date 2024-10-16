86bee9b78e50fb63c3b40cc43518a83b99b9c124
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-10-17 00:34:16 +0300

    initial commit

---
+++ backend/shareholderPractice/src/main/java/com/example/shareholderPractice/ShareholderPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shareholderPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareholderPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareholderPracticeApplication.class, args);
+	}
+
+}
---
+++ backend/shareholderPractice/src/test/java/com/example/shareholderPractice/ShareholderPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareholderPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareholderPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
